0b2cbbf37fb7eaf25ab04209e2bf9d55efe59d09
MohamedElHussein12 <mohussein539@gmail.com>
2026-06-19 19:08:05 +0300

    added outline button and edit custom button

--- lib/coustm_primary_button.dart
+++ lib/coustm_primary_button.dart
@@ -5,6 +5,8 @@ class CoustmPrimaryButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return ElevatedButton(onPressed: () {},
+     child: Text('data'),
+     );
   }
 }
