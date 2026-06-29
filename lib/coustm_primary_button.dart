32afde5d63b9f9a5e9898e992f4d0d835d5666a6
MohamedElHussein12 <mohussein539@gmail.com>
2026-06-29 13:59:45 +0300

    updated button

--- lib/coustm_primary_button.dart
+++ lib/coustm_primary_button.dart
@@ -2,11 +2,32 @@ import 'package:flutter/material.dart';
 
 class CoustmPrimaryButton extends StatelessWidget {
   const CoustmPrimaryButton({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(onPressed: () {},
-     child: Text('data'),
+    return ElevatedButton(
+  onPressed: () {
+    print("Button Clicked");
+  },
+  style: ElevatedButton.styleFrom(
+    backgroundColor: Colors.blue,
+    foregroundColor: Colors.white,
+    padding: EdgeInsets.symmetric(
+      horizontal: 40,
+      vertical: 15,
+    ),
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(12),
+    ),
+    elevation: 5,
+  ),
+  child: Text(
+    "Click Me",
+    style: TextStyle(
+      fontSize: 18,
+      fontWeight: FontWeight.bold,
+    ),
+  ),
+
      );
   }
 }
